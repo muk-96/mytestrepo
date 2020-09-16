3cad8a195c5b9f52854e1d613f03da6a35d58354
mukul <f2015583p@alumni.bits-pilani.ac.in>
2020-09-16 11:34:22 +0000

    new helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 class HelloWorld{
 	public static void main(String[] args)
 	{
-		for(int i=0; i<5; i++)
+		for(int i=0; i<3; i++)
 			System.out.println("Hello World in the crazy-experiment branch\n");
 	}
 }
